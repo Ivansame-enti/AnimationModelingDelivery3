7038494a21d93a8f2dacc35517a030e6aae0d972
Pablo <pperpinyanc@gmail.com>
2022-12-12 09:47:53 +0100

    ta mal

--- Assets/MovingBall.cs
+++ Assets/MovingBall.cs
@@ -10,15 +10,22 @@ public class MovingBall : MonoBehaviour
     //movement speed in units per second
     [Range(-1.0f, 1.0f)]
     [SerializeField]
-    private float _movementSpeed = 5f;
-    private float _AngularVeolocity = 1f;
-    private float AirResistance = 0.5f;
-    Vector3 _dir;
+    private float _movementSpeed = 10f;
 
+    public Transform ballTarget;
+    private bool _shootBall = false;
+    public Vector3 ballSpeed = new Vector3(5,0,0);
+    private Vector3 ballDirection;
+    private float radius = 0.5f;
+    private float airResistance = 0.8f;
+    private Vector3 angularVelocity =new Vector3(5,5,0);
+    private Vector3 ballDirectionMagnus;
+    private float Magnitude1,Magnitude2,Magnitude3;
+    private Vector3 Magnitude;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -30,15 +37,30 @@ public class MovingBall : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         //get the Input from Vertical axis
         float verticalInput = Input.GetAxis("Vertical");
-        Debug.Log(horizontalInput);
-        Debug.Log(verticalInput);
+
         //update the position
         transform.position = transform.position + new Vector3(-horizontalInput * _movementSpeed * Time.deltaTime, verticalInput * _movementSpeed * Time.deltaTime, 0);
 
     }
 
+    private void FixedUpdate()
+    {
+        if (_shootBall) {
+            GetComponent<Rigidbody>().AddForce(ballDirection.normalized * Magnitude.x);
+            GetComponent<Rigidbody>().AddForce(ballDirection.normalized * ballSpeed.x);
+          
+        };
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         _myOctopus.NotifyShoot();
+        _shootBall = true;
+        ballDirection = ballTarget.position - this.transform.position;
+        ballDirectionMagnus = Vector3.Cross(angularVelocity,ballSpeed);
+        Magnitude1 = ballDirectionMagnus.x * airResistance;
+        Magnitude2 = ballDirectionMagnus.y * airResistance;
+        Magnitude3 = ballDirectionMagnus.z * airResistance;
+        Magnitude = new Vector3(Magnitude1, Magnitude2, Magnitude3);
     }
 }
